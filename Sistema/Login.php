a24054221696aa274b7785c403a88dae00929f2f
joaovitor32 <joaovitormunizlopes@gmail.com>
2019-03-20 22:56:50 -0300

    Pasta Sistema adicionada

---
+++ Sistema/Login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html> 
+    <head>
+        <title>Cadastro</title>
+        <link rel="stylesheet" type="text/css" href="Css/Login.css">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 offset-3">
+                <img class="imgForm" src="Img/logoserra.png">
+                <div class="boxForm">
+                    <form method="POST">
+                        <label for="User">Login:</label>
+                        <input type="text">
+                        <label>Senha:</label>
+                        <input type="password">
+                        <input class="inputEnviar" type="submit" value="Entrar">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="Membro.js"></script>
+</body>
