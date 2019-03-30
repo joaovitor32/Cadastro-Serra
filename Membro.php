491058b26d4b9fc0a0431c2d7d31a14304f9e600
joaovitor32 <joaovitormunizlopes@gmail.com>
2019-03-29 22:18:18 -0300

    View membro

--- Membro.php
+++ Membro.php
@@ -3,6 +3,7 @@
     <head>
         <title>Cadastro</title>
         <link rel="stylesheet" type="text/css" href="Css/Membro.css">
+        <link rel="stylesheet" type="text/css" href="Css/Folha.css">
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
@@ -10,20 +11,56 @@
     </head>
     <!---Barra lateral-->
 <body>
-    <img class="imgBoxTopoCel" src="Img/logoserra.png">
-    <div class="floatBox">
-        <img class="imgBoxTopo" src="Img/logoserra.png">
-        <a href="#">Membro</a>
-        <a href="#">Projetos</a>
-        <a href="#">Ações</a>
-        <a href="#">Eventos</a>
-        <a href="#">Treinamento</a>
-    </div>
-    <div class="displayBox">
-        <form action="Busca.php" methof="POST">
-            <label class="textInput">Nome:<label>
-            <input class="inputNome" type="text" name="Nome">
-        </form>
-    </div>
+    <section>
+        <img class="imgBoxTopoCel" src="Img/logoserra.png">
+        <div class="floatBox">
+            <img class="imgBoxTopo" src="Img/logoserra.png">
+            <a href="#">Membro</a>
+            <a href="#">Projetos</a>
+            <a href="#">Ações</a>
+            <a href="#">Eventos</a>
+            <a href="#">Treinamento</a>
+        </div>
+        <div class="displayBox">
+            <form action="Busca.php" methof="POST">
+                <label class="textInput">Nome:<label>
+                <input class="inputNome" type="text" name="Nome">
+            </form>
+        </div>
+    </section>
+    <section class="viewForm">
+        <div class="container boxFormulario">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section1">
+                        <div>
+                            <img class="imgMembro" src="Img/download.jpeg">
+                        </div>
+                        <div class="box1">
+                            <div><strong>Nome:</strong><span> João Vitor Muniz Lopes</span></div>
+                            <div><strong>Curso:</strong><span> Engenharia da Computação</span></div>
+                            <div><strong>Ano de entrada:</strong><span> 01/01/1997</span></div>
+                            <div><strong>Cargo:</strong><span> Gerente</span></div>
+                            <div><strong>RG:</strong><span> 213131322123</span></div>
+                            <div><strong>CPF:</strong><span> 17979807774</span></div>
+                        </div>
+                        <div class="box2">
+                            <div><strong>Rua:</strong><span> José Aristides Pereira</span></div>
+                            <div><strong>Número:</strong><span> 13</span></div>
+                            <div><strong>Complemento:</strong><span> Lote</span></div>
+                            <div><strong>Status na empresa:</strong><span> Ativo</span></div>
+                        </div>
+                    </div>
+                </div>    
+            <div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section2">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
     <script src="Membro.js"></script>
 </body>
