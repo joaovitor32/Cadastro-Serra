f0559cce049c2ce6df93d15630f289662022b0e8
joaovitor32 <joaovitormunizlopes@gmail.com>
2019-05-18 11:50:31 -0300

    Complemento do ajax

--- Php/Sistema/SistemaGerencia/Cadastrar/Js/ProjetoCadastrar.js
+++ Php/Sistema/SistemaGerencia/Cadastrar/Js/ProjetoCadastrar.js
@@ -8,30 +8,28 @@ function addField(){
         }
         for(let i=0;i<number;i++){
             var divInputs=document.createElement("div");
-            divInputs.innerHTML="<label class='labelMembro'>Membro:</label><input class='inputMembro' type='text' onkeyup='buscarMembroTimer(this.value)' name='membro[]'>"
+            divInputs.innerHTML="<label class='labelMembro'>Membro:</label><input class='inputMembro'  autocomplete='off' type='text' onkeypress='buscarMembroTimer(this.value,"+i+")' name='membro[]'>"
             container.appendChild(divInputs);
         }
-    } 
-       
+    }  
 }
-function buscarMembroTimer(Nome){
-    setInterval(buscarMembro(Nome),1);
+function buscarMembroTimer(Nome,posInput){
+    setInterval(buscarMembro(Nome,posInput),1000);
 }
-function buscarMembro(Nome){
+function buscarMembro(Nome,posInput){
     if(window.XMLHttpRequest){
         req = new XMLHttpRequest();
     }else{
         req = new ActiveXObject("Microsoft.XMLHTTP");
     }
-    var resultado=document.getElementById("resultado");
     var inputMembro=document.getElementsByClassName("inputMembro");
     var url="./buscarMembroJs.php?valor="+Nome;
     req.open("Get",url,true);
     req.onreadystatechange=function(){
         if(req.readyState==4 && req.status==200){
-            var resposta="<div>"+req.responseText+"</div>";
+            var resposta=req.responseText;
         }   
-        resultado.innerHTML=resposta;
+        inputMembro[posInput].value=resposta;
     }
     req.send(null);
 }
